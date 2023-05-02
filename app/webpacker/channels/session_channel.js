c6158682cb22717c53cc21b7ce18c204adbc50e4
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2023-05-02 22:48:45 +0100

    Switch to tighter Channel scoping...
    This is not a normal pattern for setting up ActionCable channels, so it might need some notes. It ensures the broadcasts from the ReportJob are unique not just to the user session but also to the specific tab in the user's browser. Otherwise if the user has two different report pages open in separate tabs with the same session, the broadcast would overwrite the #report-table element in both of them.

--- app/webpacker/channels/session_channel.js
+++
@@ -1,8 +0,0 @@
-import consumer from "./consumer";
-import CableReady from "cable_ready";
-
-consumer.subscriptions.create("SessionChannel", {
-  received(data) {
-    if (data.cableReady) CableReady.perform(data.operations);
-  },
-});
