8eee2de48391de6051647180be7a48d8f06ce9b6
Maikel Linke <maikel@email.org.au>
2024-03-20 16:35:19 +1100

    Create BackorderJob to place wholesale orders

---
+++ app/jobs/backorder_job.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class BackorderJob < ApplicationJob
+  queue_as :default
+
+  def self.check_stock(order)
+    variants_needing_stock = order.variants.select do |variant|
+      variant.on_hand.negative?
+    end
+
+    linked_variants = variants_needing_stock.select do |variant|
+      variant.semantic_links.present?
+    end
+
+    linked_variants.each do |variant|
+      needed_quantity = -1 * variant.on_hand
+      # create DFC Order
+      # post order to endpoint
+    end
+  end
+
+  def perform(*args)
+    # Do something later
+  end
+end
