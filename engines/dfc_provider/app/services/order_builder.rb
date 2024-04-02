81a111dec68ad79ee3b24387bce24b40b87a034a
Maikel Linke <maikel@email.org.au>
2024-04-02 15:20:49 +1100

    WIP: Create DFC Order on other platform

--- engines/dfc_provider/app/services/order_builder.rb
+++ engines/dfc_provider/app/services/order_builder.rb
@@ -6,38 +6,8 @@ class OrderBuilder < DfcBuilder
 
     DataFoodConsortium::Connector::Order.new(
       id,
-      number: ofn_order.number,
-      date: ofn_order.completed_at.to_s,
       client: urls.enterprise_url(ofn_order.distributor_id),
-      lines: order_lines(ofn_order),
       # TODO: FulfillementState: held
     )
-  end
-
-  def self.order_lines(ofn_order)
-    ofn_order.line_items.map(&method(:order_line))
-  end
-
-  def self.order_line(line_item)
-    DataFoodConsortium::Connector::OrderLine.new(
-      nil,
-      quantity: line_item.quantity,
-      offer: best_offer(line_item),
-    )
-  end
-
-  def self.best_offer(line_item)
-    link = line_item.variant.semantic_links[0]
-
-    return unless link
-
-    importer = WebImporter.new(line_item.order.distributor.owner)
-    catalog = importer.import(link.semantic_id)
-
-    # WIP: possibly add more to the catalog, resolving more URIs
-    catalog.find do |item|
-      # Might there be multiple?
-      item.is_a?(DataFoodConsortium::Connector::Offer)
-    end
   end
 end
--- spec/jobs/backorder_job_spec.rb
+++ spec/jobs/backorder_job_spec.rb
@@ -10,4 +10,85 @@ RSpec.describe BackorderJob do
       BackorderJob.check_stock(order)
     end
   end
+
+  describe ".best_offer" do
+    # This is a copy from our API docs.
+    # But once available, we can record a real response.
+    let(:catalog_json) {
+      <<~JSON
+        {
+          "@context": "https://www.datafoodconsortium.org",
+          "@graph": [
+            {
+              "@id": "http://test.host/api/dfc/persons/12345",
+              "@type": "dfc-b:Person",
+              "dfc-b:affiliates": "http://test.host/api/dfc/enterprises/10000"
+            },
+            {
+              "@id": "http://test.host/api/dfc/enterprises/10000",
+              "@type": "dfc-b:Enterprise",
+              "dfc-b:hasAddress": "http://test.host/api/dfc/addresses/40000",
+              "dfc-b:name": "Fred's Farm",
+              "dfc-b:hasDescription": "Beautiful",
+              "dfc-b:manages": "http://test.host/api/dfc/enterprises/10000/catalog_items/10001",
+              "dfc-b:supplies": "http://test.host/api/dfc/enterprises/10000/supplied_products/10001",
+              "ofn:long_description": "<p>Hello, world!</p><p>This is a paragraph.</p>"
+            },
+            {
+              "@id": "http://test.host/api/dfc/enterprises/10000/catalog_items/10001",
+              "@type": "dfc-b:CatalogItem",
+              "dfc-b:references": "http://test.host/api/dfc/enterprises/10000/supplied_products/10001",
+              "dfc-b:sku": "AR",
+              "dfc-b:stockLimitation": 0,
+              "dfc-b:offeredThrough": "http://test.host/api/dfc/enterprises/10000/offers/10001"
+            },
+            {
+              "@id": "http://test.host/api/dfc/enterprises/10000/supplied_products/10001",
+              "@type": "dfc-b:SuppliedProduct",
+              "dfc-b:name": "Apple - 1g",
+              "dfc-b:description": "Red",
+              "dfc-b:hasType": "dfc-pt:non-local-vegetable",
+              "dfc-b:hasQuantity": {
+                "@type": "dfc-b:QuantitativeValue",
+                "dfc-b:hasUnit": "dfc-m:Gram",
+                "dfc-b:value": 1
+              },
+              "dfc-b:alcoholPercentage": 0,
+              "dfc-b:lifetime": "",
+              "dfc-b:usageOrStorageCondition": "",
+              "dfc-b:totalTheoreticalStock": 0,
+              "ofn:spree_product_id": 90000,
+              "ofn:spree_product_uri": "http://test.host/api/dfc/enterprises/10000?spree_product_id=90000"
+            },
+            {
+              "@id": "http://test.host/api/dfc/enterprises/10000/offers/10001",
+              "@type": "dfc-b:Offer",
+              "dfc-b:hasPrice": 19.99,
+              "dfc-b:stockLimitation": 0
+            }
+          ]
+        }
+      JSON
+    }
+
+    it "finds a linked offer" do
+      dfc_user = order.distributor.owner
+      variant = order.line_items[0].variant
+
+      dfc_user.oidc_account = OidcAccount.new(
+        uid: "me",
+        token: "xxx_token_xxx",
+      )
+      variant.semantic_links << SemanticLink.new(
+        semantic_id: "https://example.net/product-6"
+      )
+      stub_request(:get, "https://example.net/product-6").to_return(
+        status: 200, body: catalog_json
+      )
+
+      offer = BackorderJob.best_offer(dfc_user, variant)
+
+      expect(offer.semanticId).to eq "http://test.host/api/dfc/enterprises/10000/offers/10001"
+    end
+  end
 end
