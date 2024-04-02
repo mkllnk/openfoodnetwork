18dcac53d823a478983ab6e194af6ba041a6f78d
Maikel Linke <maikel@email.org.au>
2024-04-02 15:20:49 +1100

    WIP: Create DFC Order on other platform

--- app/jobs/backorder_job.rb
+++ app/jobs/backorder_job.rb
@@ -12,14 +12,60 @@ class BackorderJob < ApplicationJob
       variant.semantic_links.present?
     end
 
+    return if linked_variants.empty?
+
+    # At this point we want to move to the background with perform_later.
+    # But while this is in development I'll perform the backordering
+    # immediately. It should ease debugging for now.
+
+    backorder = build_order(order)
+
     linked_variants.each do |variant|
       needed_quantity = -1 * variant.on_hand
-      # create DFC Order
-      # post order to endpoint
+      offer = best_offer(order.distributor.owner, variant)
+      backorder.lines << build_order_line(offer, needed_quantity)
+    end
+
+    json = DfcIo.export(backorder, *backorder.lines)
+
+    api = DfcRequest.new(order.distributor.owner)
+
+    # TODO: delete old order if exists
+    # Create order via POST:
+    api.call("https://example.net/orders", json)
+
+    # Once we have transformations and know the quantities in bulk products
+    # we will need to increase on_hand by the ordered quantity.
+    linked_variants.each do |variant|
+      variant.on_hand = 0
+    end
+  end
+
+  # This needs to find an existing order when the API is available.
+  def self.build_order(ofn_order)
+    OrderBuilder.new_order(ofn_order)
+  end
+
+  def self.build_order_line(offer, quantity)
+    OrderLineBuilder.build(offer, quantity)
+  end
+
+  def self.best_offer(user, variant)
+    link = variant.semantic_links[0]
+
+    return unless link
+
+    importer = WebImporter.new(user)
+    catalog = importer.import(link.semantic_id)
+
+    # WIP: possibly add more to the catalog, resolving more URIs
+    catalog.find do |item|
+      # Might there be multiple?
+      item.is_a?(DataFoodConsortium::Connector::Offer)
     end
   end
 
   def perform(*args)
-    # Do something later
+    # The ordering logic will live here later.
   end
 end
