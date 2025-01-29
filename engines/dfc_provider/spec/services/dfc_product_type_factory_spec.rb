9bb01984a32aebd4aa54232b966f2a101f1fc1e0
Maikel Linke <maikel@email.org.au>
2025-01-29 14:35:02 +1100

    DRY product type spec

--- engines/dfc_provider/spec/services/dfc_product_type_factory_spec.rb
+++ engines/dfc_provider/spec/services/dfc_product_type_factory_spec.rb
@@ -4,43 +4,33 @@ require_relative "../spec_helper"
 
 RSpec.describe DfcProductTypeFactory do
   describe ".for" do
-    let(:dfc_id) {
-      "https://github.com/datafoodconsortium/taxonomies/releases/latest/download/productTypes.rdf#drink"
-    }
-
     it "assigns a top level product type" do
-      expect(described_class.for(dfc_id))
+      expect(described_class.for(dfc_id("drink")))
         .to eq DfcLoader.connector.PRODUCT_TYPES.DRINK
     end
 
     context "with second level product type" do
-      let(:dfc_id) {
-        "https://github.com/datafoodconsortium/taxonomies/releases/latest/download/productTypes.rdf#soft-drink"
-      }
-
       it "assigns a second level product type" do
-        expect(described_class.for(dfc_id))
+        expect(described_class.for(dfc_id("soft-drink")))
           .to eq DfcLoader.connector.PRODUCT_TYPES.DRINK.SOFT_DRINK
       end
     end
 
     context "with leaf level product type" do
-      let(:dfc_id) {
-        "https://github.com/datafoodconsortium/taxonomies/releases/latest/download/productTypes.rdf#lemonade"
-      }
-
       it "assigns a leaf level product type" do
-        expect(described_class.for(dfc_id))
+        expect(described_class.for(dfc_id("lemonade")))
           .to eq DfcLoader.connector.PRODUCT_TYPES.DRINK.SOFT_DRINK.LEMONADE
       end
     end
 
     context "with non existing product type" do
-      let(:dfc_id) { "other" }
-
       it "returns nil" do
-        expect(described_class.for(dfc_id)).to be_nil
+        expect(described_class.for("other")).to be_nil
       end
     end
+  end
+
+  def dfc_id(name)
+    "https://github.com/datafoodconsortium/taxonomies/releases/latest/download/productTypes.rdf##{name}"
   end
 end
