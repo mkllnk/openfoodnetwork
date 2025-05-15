b6237b14cfd2d6f6d846a8b40eb61b37bc6e55a3
Maikel Linke <maikel@email.org.au>
2025-05-15 15:52:52 +1000

    Add DfcPermission model to persist granted scopes

---
+++ app/models/dfc_permission.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Authorisations of a user allowing a platform to access to data.
+class DfcPermission < ApplicationRecord
+  SCOPES = %w[ReadEnterprise ReadProducts ReadOrders].freeze
+
+  belongs_to :user, class_name: "Spree::User"
+  belongs_to :enterprise
+
+  validates :grantee, presence: true
+  validates :scope, presence: true, inclusion: { in: SCOPES }
+end
