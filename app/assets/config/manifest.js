56625d93ae176a706aa17301c032600dcab41cb8
Maikel Linke <maikel@email.org.au>
2025-08-14 16:14:34 +1000

    Bump sprockets from 3.7.2 to 4.2.2
    The upgrade requires a new manifest file that replaces the old list of
    additional assets.

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,10 @@
+// app/assets/config/manifest.js
+
+// link_tree ../javascripts/admin
+//= link admin/all.js
+//= link admin_minimal.js
+//= link web/all.js
+//= link darkswarm/all.js
+//= link_directory ../javascripts/shared .js
+
+//= link mail.scss
